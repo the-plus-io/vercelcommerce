e3a6bcd5ab175272605d4ba5bf2310b8cef6479d
the-plus-io <hi@the-plus.io>
2024-09-09 18:20:34 +0200

    Auto go to next step

--- components/SolarPlanningForm.tsx
+++ components/SolarPlanningForm.tsx
@@ -3,13 +3,13 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -52,6 +52,8 @@ export function SolarPlanningForm() {
         const place = autocomplete.getPlace()
         if (place.formatted_address) {
           form.setValue("address", place.formatted_address)
+          // Automatically submit the form when an address is selected
+          form.handleSubmit(onSubmit)()
         }
       })
     }
