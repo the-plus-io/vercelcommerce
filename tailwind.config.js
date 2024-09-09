536f9e301e0dfce7de2f4ea186d9b9dca27ab313
the-plus-io <hi@the-plus.io>
2024-09-09 10:50:36 +0200

    Revert "added form"
    This reverts commit 12340ad2eb18d41dd01003cd1f5cbe5d3d256ffa.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,54 +2,33 @@ const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ['class'],
-    content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: ['var(--font-geist-sans)']
-  		},
-  		keyframes: {
-  			fadeIn: {
-  				from: {
-  					opacity: '0'
-  				},
-  				to: {
-  					opacity: '1'
-  				}
-  			},
-  			marquee: {
-  				'0%': {
-  					transform: 'translateX(0%)'
-  				},
-  				'100%': {
-  					transform: 'translateX(-100%)'
-  				}
-  			},
-  			blink: {
-  				'0%': {
-  					opacity: '0.2'
-  				},
-  				'20%': {
-  					opacity: '1'
-  				},
-  				'100% ': {
-  					opacity: '0.2'
-  				}
-  			}
-  		},
-  		animation: {
-  			fadeIn: 'fadeIn .3s ease-in-out',
-  			carousel: 'marquee 60s linear infinite',
-  			blink: 'blink 1.4s both infinite'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)']
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 }
+        },
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' }
+        },
+        blink: {
+          '0%': { opacity: 0.2 },
+          '20%': { opacity: 1 },
+          '100% ': { opacity: 0.2 }
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn .3s ease-in-out',
+        carousel: 'marquee 60s linear infinite',
+        blink: 'blink 1.4s both infinite'
+      }
+    }
   },
   future: {
     hoverOnlyWhenSupported: true
@@ -70,7 +49,6 @@ module.exports = {
           values: theme('transitionDelay')
         }
       );
-    }),
-      require("tailwindcss-animate")
-]
+    })
+  ]
 };
